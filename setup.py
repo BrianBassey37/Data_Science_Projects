2a14156672450c5156c507f8d0da9021172532f1
brianbassey37 <brianbassey37@gmail.com>
2024-01-14 22:56:11 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repo contains a catalogue of all my projects during my internship at',
+    author='Brian Edem Bassey',
+    license='MIT',
+)
